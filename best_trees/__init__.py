97d801fb6097065b762aacab313a10879eb873db
Jonathan Chang <jchang641@gmail.com>
2015-03-27 19:35:05 -0700

    Add burnin option

--- best_trees/__init__.py
+++ best_trees/__init__.py
@@ -24,8 +24,9 @@ __version__ = '0.1'
 
 @argh.arg('treefile', help='.trees file to load')
 @argh.arg('--max-trees', help='number of trees to retrieve')
+@argh.arg('--burnin', help='number of trees to remove as burnin')
 @argh.arg('--output', help='name of output file')
-def cmd(treefile, max_trees=100, output="output.bestn.trees"):
+def cmd(treefile, max_trees=100, burnin=0, output="output.bestn.trees"):
     """Gets the best N trees based on posterior probability from a BEAST 1.x treelog.
     """
     # initialize heap with all small numbers
@@ -35,17 +36,19 @@ def cmd(treefile, max_trees=100, output="output.bestn.trees"):
     # keep track of nexus file state
     pre = []
     post = []
-    seen_tree = False
+    trees_seen = 0
 
     # read the tree
     with open(treefile) as rfile:
         for line in rfile:
             if line.startswith('tree'): # inside tree
-                seen_tree = True
+                trees_seen += 1
+                if trees_seen <= burnin:
+                    continue
                 _, state, comment, tree = line.split(" ", 3)
                 prob = float(re.search('posterior=(.+)\]', comment).group(1))
                 heapq.heappushpop(heap, (prob, state, comment, tree))
-            elif seen_tree: # after trees
+            elif trees_seen > 0: # after trees
                 post.append(line)
             else: # before trees
                 pre.append(line)
